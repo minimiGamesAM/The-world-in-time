1079b38a7c7e7d08fc63f4055371ba0482156df1
minimiGamesA <minimiGamesAM@GMAIL.COM>
2021-01-26 10:15:54 -0500

    add gravity_AM
    Now the objet has attraction for the planet

---
+++ Assets/Script/Attractor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach(Attractor attractor in attractors)
+        {
+            if(attractor  != this)
+                Attract(attractor);
+
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Attract(Attractor objtoAttract)
+    {
+        Rigidbody rbToAttract = objtoAttract.rb;
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+        
+
+
+    }
+}
