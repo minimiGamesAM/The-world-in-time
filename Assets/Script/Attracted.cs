49619bc86310f852be66443d8a7dfbca6e9a8d02
minimiM <minimiGamesAM@gmail.com>
2021-01-28 21:05:31 -0500

    World: Add gravity and some player controller.

---
+++ Assets/Script/Attracted.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attracted : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Rigidbody _atractorRb;
+    public GameObject _atractorGameObject;
+
+    public static float G = 0.00000667f;
+
+    void FixedUpdate()
+    {
+
+        Attract(_atractorRb);
+
+    }
+
+    void Attract(Rigidbody rbAttractor)
+    {
+        Vector3 direction = _rb.position - rbAttractor.position;
+        float distance = direction.magnitude;
+        float forceMagnitud = G * _rb.mass * rbAttractor.mass / Mathf.Pow(distance, 2);
+        Vector3 force = -direction.normalized * forceMagnitud;
+        _rb.AddForce(force);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _atractorRb = _atractorGameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public GameObject _atractorGameObject;
+    private Vector3 _moveVelocityDir;
+
+    public float _playerSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetDir = (_atractorGameObject.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.FromToRotation(transform.up, targetDir) * transform.rotation;
+
+        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        _moveVelocityDir = Vector3.forward * verticalMovement + Vector3.right * horizontalMovement;
+    }
+
+    void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + transform.TransformDirection(_moveVelocityDir * _playerSpeed * Time.fixedDeltaTime));
+    }
+}
