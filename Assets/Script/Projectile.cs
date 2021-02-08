b1227c14601730648f709e438384d83884027b37
minimiM <minimiGamesAM@gmail.com>
2021-02-07 19:53:02 -0500

    Worlds: Adding planets with same among of mass.

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -7,10 +7,12 @@ public class Projectile : MonoBehaviour
     private float _timeLife = -1.0f;
     private float MAX_LIFE = 5f;
 
+    private Vector3 _currentPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentPoint = transform.position;
     }
 
     // Update is called once per frame
@@ -25,6 +27,15 @@ public class Projectile : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void FixedUpdate()
+    {
+        // always draw a 5-unit colored line from the origin
+        Color color = new Color(0.5f, 0.4f, 1.0f);
+        Debug.DrawLine(_currentPoint, transform.position, color, 30);
+
+        _currentPoint = transform.position;
     }
 
     void OnCollisionEnter(Collision collision)
